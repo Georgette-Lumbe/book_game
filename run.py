e0acd967b1a9f87a356ca7dc80483d18196d0ff8
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-07-18 13:57:37 +0000

    Import time and set welcome function

--- run.py
+++ run.py
@@ -1,8 +1,26 @@
 # Import
+import time
 
-# Get user data
 
-# Start game 
+def welcome():
+    """
+    Welcome and get player data
+    """
+    print('Hello there!')
+    time.sleep(1)
+
+    player_name = input('What is your name?\n').capitalize()
+    time.sleep(2)
+    print(f"Welcome to the children's reading camp {player_name}")
+    time.sleep(2)
+    print("Happy to have you here. So, let's start the game\n")
+    time.sleep(1)
+
+
+welcome()
+
+
+# Function to start the game 
 
 # Fisrt option
 
