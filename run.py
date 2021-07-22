0e2940d57264f264dbce2b752acfa3f152829802
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-07-22 21:45:44 +0000

    Add a Class for user data, delete arguments in all functions and fix indents errors

--- run.py
+++ run.py
@@ -17,62 +17,75 @@ while True:
     break
 
 
+class Data:
+
+    def getData(self, player_name, child_name):
+        Data.player_name = player_name
+        Data.child_name = child_name
+
+
 def welcome():
     """
     Welcome and get player data
     """
-    print('Hello there!')
-    time.sleep(1)
-
-    player_name = input('What is your name?\n').capitalize()
     time.sleep(2)
     # Style the welcome message
     welcome_art = figlet_format(f"Welcome {player_name}", font='slant')
     colored_art = colored(welcome_art, 'cyan', attrs=['bold'])
     print(colored_art)
     time.sleep(2)
-    print(colored("Happy to have you here. So, let's start\n", 'cyan'))
+    print("Happy to have you here. So, let's start\n")
     time.sleep(1)
-    start_game(player_name)
+
+
+player_name = input(colored("What is your name?\n",
+                    'cyan', attrs=['bold'])).capitalize()
+welcome()
 
 # Function to start the game
 
 
-def start_game(player_name):
+def start_game():
     """
     start game, set scenario, get child name and type of books
     """
-    print("Your child has to give a presentation on an interesting book "
+    time.sleep(1)
+    print('\nPerfect!')
+    time.sleep(2)
+    print(f"{child_name} has to give a presentation on an interesting book "
           "in front of parents, teachers and the whole class.")
     time.sleep(0.5)
     print("The book should be appropriate and comprehensible.\n")
-    time.sleep(2)
-    child_name = input("What is your child's name?\n").capitalize()
-    time.sleep(1)
-    print('\nPerfect!')
     time.sleep(1.5)
     print(f"You should make a choice for {child_name}, be careful, "
           "make the right choice!\n")
     time.sleep(2)
     print(f"{player_name}, there are 4 types of books.\n")
+    time.sleep(1)
     # Style types of books
     print(("  "), (colored("ADVENTURE", "green", attrs=["reverse", "bold"])),
                   (colored("ROMANTIC", "red", attrs=["reverse", "bold"])),
                   (colored("COMEDY", "yellow", attrs=["reverse", "bold"])),
                   (colored("HISTORY", "cyan", attrs=["reverse", "bold"])))
+    time.sleep(1.5)
 
-    book_choice(player_name, child_name)
+
+child_name = input(colored("What is your child's name?\n",
+                   'cyan', attrs=['bold'])).capitalize()
 
 
-def book_choice(player_name, child_name):
+def book_choice():
     """
-    Function about book choice for the user
+    Helper Function about user' book choice
+    A while loop to check user' input
+    try and except for ValueError
     """
     book_choice_options = ['a', 'r', 'c', 'h']
     user_choice = ''
     while user_choice not in book_choice_options:
-        print('\nWhich type of book will you choose?')
-        user_choice = input('Enter a letter: (a, r, c or h)\n')
+        print(colored('\nWhich type of book will you choose?'
+                      '(a, r, c or h)', 'cyan', attrs=['bold']))
+        user_choice = input('Enter a letter: ')
         break
     try:
         if user_choice not in book_choice_options:
@@ -80,35 +93,38 @@ def book_choice(player_name, child_name):
                 f"You provide wrong data {user_choice}"
             )
     except ValueError as e:
-        print(f'Invalid data: {e}, please try again\n')
-        book_choice(player_name, child_name)
+        print(colored(f'Invalid : {e}, please try again',
+                      'red', attrs=['bold']))
+        book_choice()
 
     if user_choice == book_choice_options[0]:
         # if the player typed a, he will redirect to adventure_book()
-        adventure_book(player_name, child_name)
+        adventure_book()
     elif user_choice == book_choice_options[1]:
         # if the player typed r, he will redirect to romantic_book()
-        romantic_book(player_name, child_name)
+        romantic_book()
     elif user_choice == book_choice_options[2]:
         # if the player typed c, he will redirect to comedy_book()
-        comedy_book(player_name, child_name)
+        comedy_book()
     elif user_choice == book_choice_options[3]:
         # if the player typed h, he will redirect to history_book()
-        history_book(player_name, child_name)
+        history_book()
 
 
-def validate_integer(player_name, child_name):
+def validate_integer():
     """
-    Function for validate integer data
+    Helper Function for user' validate data
+    Use of While loop, try and except
     If the user enter an incorrect data,
-    he will ask to re-enter another data again
+    he will ask to re-enter data again
     """
     user_integer_options = ['1', '2']
     user_integer = ' '
     while user_integer not in user_integer_options:
-        print(colored(f'What would you choose {player_name} (1 or 2)?',
+        print(colored(f'\nWhat would you choose {player_name} (1 or 2)?',
                       'cyan', attrs=['bold']))
         user_integer = input('Enter a number: ')
+        time.sleep(1.5)
         break
     try:
         if user_integer not in user_integer_options:
@@ -116,12 +132,15 @@ def validate_integer(player_name, child_name):
                 f"You provide wrong data {user_integer}"
             )
     except ValueError as e:
-        print(f'Invalid : {e}, please try again')
-        validate_integer(player_name, child_name)
+        print(colored(f'Invalid : {e}, please try again',
+                      'red', attrs=['bold']))
+        # Call again validate_integer if user enter wrong data
+        validate_integer()
+
     if user_integer == user_integer_options[0]:
         # Call game over function when it is Bad Choice
         time.sleep(2)
-        game_over(player_name, child_name)
+        game_over()
     elif user_integer == user_integer_options[1]:
         # Good Choice
         # Style the output
@@ -140,11 +159,11 @@ def validate_integer(player_name, child_name):
 # Adventure book function
 
 
-def adventure_book(player_name, child_name):
+def adventure_book():
     """
     Description of situation
     """
-    print('Yay!! You have chosen the Adventure Book')
+    print('\nYay!! You have chosen the Adventure Book')
     time.sleep(1)
     print("Let's bring some adventure into this presentation\n")
     time.sleep(2)
@@ -156,9 +175,11 @@ def adventure_book(player_name, child_name):
     time.sleep(1)
     print('Choice 2: The Ship of Shadows\n')
     time.sleep(1.5)
-    validate_integer(player_name, child_name)
+    # If user enter wrong data
+    validate_integer()
 
     time.sleep(1)
+
     print(colored('Do you need a summary of this book? (yes or no)',
                   'cyan', attrs=['bold']))
     time.sleep(1)
@@ -174,26 +195,25 @@ def adventure_book(player_name, child_name):
               "legend. ... But life aboard the Ship of Shadows is more  "
               "than even she bargained for.")
         time.sleep(2)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
     elif 'no' in choice:
         print("It's perfect, you can find out for yourself by reading it")
         time.sleep(1.5)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
     else:
         print('OOPS, Invalid Choice')
-
         time.sleep(1.5)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
 
 
 # Romantic book function
 
 
-def romantic_book(player_name, child_name):
+def romantic_book():
     """
     Description of situation with differents choices
     """
-    print('Perfect!! You have chosen the Romantic Book')
+    print('\nPerfect!! You have chosen the Romantic Book')
     time.sleep(1)
     print("Let's bring some love into this presentation\n")
     time.sleep(2)
@@ -205,7 +225,8 @@ def romantic_book(player_name, child_name):
     time.sleep(1)
     print('Choice 2: Mama, Do you love me?\n')
     time.sleep(1.5)
-    validate_integer(player_name, child_name)
+    # If user enter wrong data
+    validate_integer()
     time.sleep(1)
     print(colored('Do you need a summary of this book? (yes or no)',
                   'cyan', attrs=['bold']))
@@ -220,26 +241,26 @@ def romantic_book(player_name, child_name):
               'adventure, and wonder in which a young Inuit girl disobeys '
               'wanders away from home and learns valuable lessons..')
         time.sleep(2)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
     elif 'no' in choice:
         print("It's perfect, you can find out for yourself by reading it")
         time.sleep(1.5)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
     else:
         print('OOPS, Invalid Choice')
 
         time.sleep(1.5)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
 
 
 # Comedy book function
 
 
-def comedy_book(player_name, child_name):
+def comedy_book():
     """
     Description of situation
     """
-    print('HA HA!! You have chosen the Comedy Book')
+    print('\nHA HA!! You have chosen the Comedy Book')
     time.sleep(1)
     print("Let's bring some fun into this presentation\n")
     time.sleep(2)
@@ -251,7 +272,8 @@ def comedy_book(player_name, child_name):
     time.sleep(1)
     print('Choice 2: Coco Banjo\n')
     time.sleep(1.5)
-    validate_integer(player_name, child_name)
+    # If user enter wrong data
+    validate_integer()
     time.sleep(1)
     print(colored('Do you need a summary of this book? (yes or no)',
                   'cyan', attrs=['bold']))
@@ -275,24 +297,24 @@ def comedy_book(player_name, child_name):
         time.sleep(1.5)
         print("How will she get out of this one?")
         time.sleep(2)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
     elif 'no' in choice:
         print("It's perfect, you can find out for yourself by reading it")
         time.sleep(1.5)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
     else:
         print('OOPS, Invalid Choice')
         time.sleep(2)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
 
 # History book function
 
 
-def history_book(player_name, child_name):
+def history_book():
     """
     Description of situation
     """
-    print('That is great!! You have chosen the History Book\n')
+    print('\nThat is great!! You have chosen the History Book')
     time.sleep(2)
     print(f'There are 2 choices for History Book {player_name}\n')
     time.sleep(2)
@@ -302,7 +324,8 @@ def history_book(player_name, child_name):
     time.sleep(1)
     print('Choice 2: Who was Anne Franck?\n')
     time.sleep(1.5)
-    validate_integer(player_name, child_name)
+    # If user enter wrong data
+    validate_integer()
     time.sleep(1)
     print(colored('Do you need a summary of this book? (yes or no)',
                   'cyan', attrs=['bold']))
@@ -316,20 +339,21 @@ def history_book(player_name, child_name):
         time.sleep(1)
         print("Anne died in the Bergen-Belsen camp in 1945.")
         time.sleep(2)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
     elif 'no' in choice:
         print("It's perfect, you can find out for yourself by reading it")
         time.sleep(1)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
     else:
         print('OOPS, Invalid Choice')
         time.sleep(2)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
+
 
 # Function to play again
 
 
-def make_another_choice(player_name, child_name):
+def make_another_choice():
     """
     Give user the opportunity to make another choice
     """
@@ -340,8 +364,8 @@ def make_another_choice(player_name, child_name):
     # Take the user input
     choice = input().lower()
     if 'yes' in choice:
-        # if the player choose 'yes', start the book game from beginning
-        book_choice(player_name, child_name)
+        # if the player choose 'yes', start the book game from book choice
+        book_choice()
     elif 'no' in choice:
         print('')
         time.sleep(1)
@@ -354,12 +378,12 @@ def make_another_choice(player_name, child_name):
         print('OOPS, Invalid Data')
         print(f'Please {player_name}, make another choice\n')
         time.sleep(2)
-        make_another_choice(player_name, child_name)
+        make_another_choice()
 
 # game_over function
 
 
-def game_over(player_name, child_name):
+def game_over():
     """
     If player chose wrong choice, game_over function is called
     """
@@ -374,7 +398,12 @@ def game_over(player_name, child_name):
           "it is absolutely not suitable\n")
     time.sleep(1.5)
     # Make another choice
-    make_another_choice(player_name, child_name)
+    make_another_choice()
+
+# Call functions
 
 
-welcome()
+start_game()
+book_choice()
+c1 = Data()
+c1.getData(player_name, child_name)
